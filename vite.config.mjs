2cba439b2112313bd4168080d5743e596cd66007
Yahya-Sirguroh <yahyasirguroh@gmail.com>
2026-05-04 02:05:23 +0530

    fileupdated5

---
+++ vite.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  server: {
+    proxy: {
+      // In local dev, forward /api/* → http://localhost:5000/api/*
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+  },
+})
